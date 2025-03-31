d6bdd0743700d3ba6bfc66483e089d89545fb0c6
Dilyorbek Ubaydullayev <dilyorbekubaydullayev@gmail.com>
2025-03-31 18:10:30 +0500

    main ui finished

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/shared/navbar"
+import Home from "./pages/home"
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
