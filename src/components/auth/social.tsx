ef26735a1dee35e01917b569d9706f7626bc5b97
Dilyorbek Ubaydullayev <dilyorbekubaydullayev@gmail.com>
2025-04-02 13:31:41 +0500

    firebase connected

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -1,23 +1,54 @@
-import { FaGithub, FaGoogle } from "react-icons/fa6"
-import { Button } from "../ui/button"
-import { Separator } from "../ui/separator"
+import { FaGithub, FaGoogle } from "react-icons/fa6";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "@/firebase";
+import FillLoading from "../shared/fillLoading";
 
 const Social = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const onGoogle = () => {
+    setIsLoading(true);
+    const googleProvider = new GoogleAuthProvider();
+    signInWithPopup(auth, googleProvider)
+      .then(() => {
+        navigate("/");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  const onGithub = () => {
+    setIsLoading(true);
+    const githubProvider = new GithubAuthProvider();
+    signInWithPopup(auth, githubProvider)
+      .then(() => {
+        navigate("/");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
   return (
     <>
-    <Separator className="-my-1" />
-    <div className="grid grid-cols-2 gap-2">
-        <Button className="h-11 " variant={'secondary'}>
-            <FaGithub/>
-            <span>Sign in with Github</span>
+      {isLoading && <FillLoading />}
+      <Separator className="-my-1" />
+      <div className="grid grid-cols-2 gap-2">
+        <Button className="h-11 " variant={"secondary"} disabled={isLoading} onClick={onGithub}>
+          <FaGithub />
+          <span>Sign in with Github</span>
         </Button>
-        <Button className="h-11" variant={'destructive'}>
-            <FaGoogle/>
-            <span>Sign in with Google</span>
+        <Button className="h-11" variant={"destructive"} disabled={isLoading} onClick={onGoogle}>
+          <FaGoogle />
+          <span>Sign in with Google</span>
         </Button>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
