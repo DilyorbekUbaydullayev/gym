258d91b52e0cfa8442bb0d7eaf209c15e5e0842a
Dilyorbek Ubaydullayev <dilyorbekubaydullayev@gmail.com>
2025-04-03 10:19:33 +0500

    auth provider  ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { Routes,Route } from "react-router-dom"
 import Navbar from "./components/shared/navbar"
 import Home from "./pages/home"
 import Auth from "./pages/auth"
+import Dashboard from "./pages/dashboard"
 
 
 
 
 function App() {
   return (
-    <div>
+    <div >
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/auth" element={<Auth/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
       
     </div>
