33bd264f1c8de3a3376b67f4235fcce28f7247bc
Dilyorbek Ubaydullayev <dilyorbekubaydullayev@gmail.com>
2025-04-05 09:35:10 +0500

    update/delete ready

--- src/components/shared/taskItem.tsx
+++ src/components/shared/taskItem.tsx
@@ -4,26 +4,34 @@ import { HiStatusOnline } from "react-icons/hi";
 import { CiPlay1 } from "react-icons/ci";
 import { Button } from "../ui/button";
 import { Edit2, Trash } from "lucide-react";
+import { ITask } from "@/types";
 
-const TaskItem = () => {
+
+interface Props {
+    task:ITask
+    onStartEditing:()=>void,
+    onDelete:()=>void
+}
+
+const TaskItem = ({task,onStartEditing,onDelete}:Props) => {
   return (
     <Card className="w-full p-4 shadow-md grid grid-cols-4 items-center gap-2 relative">
-        <div className="flex gap-1 items-center col-span-2 w-[50%]">
+        <div className="flex gap-2 items-center col-span-2 w-full">
             <MdOutlineTaskAlt className="text-blue-500"/>
-            <span className="capitalize">Press</span>
+            <p className="capitalize w-full">{task.title}</p>
         </div>
-        <div className="flex gap-1 items-center">
+        <div className="flex gap-1 items-center col-span-1">
             <HiStatusOnline/>
-            <span className="capitalize text-sm">Unstarted</span>
+            <span className="capitalize text-sm">{task.status}</span>
         </div>
-        <div className="flex gap-2 items-center justify-self-end">
+        <div className="flex gap-2 items-center justify-self-end col-span-1">
             <Button variant={'ghost'} size={'icon'}>
                 <CiPlay1 className="text-indigo-500 w-5 h-5"/>
             </Button>
-            <Button variant={'secondary'} size={'icon'}>
+            <Button variant={'secondary'} size={'icon'} onClick={onStartEditing}>
                 <Edit2 className=" w-5 h-5"/>
             </Button>
-            <Button variant={'destructive'} size={'icon'}>
+            <Button variant={'destructive'} size={'icon'} onClick={onDelete}>
                 <Trash className=" w-5 h-5"/>
             </Button>
         </div>
